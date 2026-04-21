c965b5d81b5497c642684aa011b8514144cbb6bb
AdrianSilva05 <adriansilva10052021@gmail.com>
2026-04-21 15:44:35 -0500

    Pruebas Funcionamiento (micrófono y amplificador)
    Códigos para probar el funcionamiento del micrófono INMP441 (comunicaciones.ino) y del amplificador MAX98357A (prueba_amp.ino). Las pruebas se deben ejecutar una por una en Arduino IDE, con las librerías Arduino Audio Tools y ESP32 A2DP previamente instaladas.

---
+++ prueba_amp/prueba_amp.ino
@@ -0,0 +1,19 @@
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+
+I2SStream i2s;
+BluetoothA2DPSink a2dp_sink(i2s);
+
+void setup() {
+    Serial.begin(115200);
+    auto cfg = i2s.defaultConfig();
+    cfg.pin_bck = 27;
+    cfg.pin_ws = 26;
+    cfg.pin_data = 25;
+    i2s.begin(cfg);
+
+    a2dp_sink.start("BLUTUDELDIABLO");
+}
+
+void loop() {
+}
